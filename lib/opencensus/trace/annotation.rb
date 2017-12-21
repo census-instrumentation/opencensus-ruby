812067b0c0d0dc1e3b8aa5c8bf573e42a6fe6e20
Jeff Ching <ching.jeff@gmail.com>
2017-12-21 15:44:07 -0800

    Builder to read only (#6)
    * SpanBuilder#to_span
    
    * Adding tests for span builder and creating read only data
    
    * Fix split to_span building into separate private methods

--- lib/opencensus/trace/annotation.rb
+++ lib/opencensus/trace/annotation.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "opencensus/trace/time_event"
+
 module OpenCensus
   module Trace
     ##

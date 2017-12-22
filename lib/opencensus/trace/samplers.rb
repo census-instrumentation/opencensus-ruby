056f6a513b550bf4f25d77bec6bfcb52604bd4a1
Daniel Azuma <dazuma@google.com>
2017-12-22 10:55:42 -0800

    Simplify and rename time sampler to match the rest of the framework

--- lib/opencensus/trace/samplers.rb
+++ lib/opencensus/trace/samplers.rb
@@ -15,12 +15,36 @@
 require "opencensus/trace/samplers/always_sample"
 require "opencensus/trace/samplers/never_sample"
 require "opencensus/trace/samplers/probability"
-require "opencensus/trace/samplers/time"
+require "opencensus/trace/samplers/max_qps"
 
 module OpenCensus
   module Trace
+    ##
+    # A sampler determines whether a given request's latency trace should
+    # actually be reported. It is usually not necessary to trace every
+    # request, especially for an application serving heavy traffic. You may
+    # use a sampler to decide, for a given request, whether to report its
+    # trace.
+    #
+    # The OpenCensus specification defines three samplers: AlwaysSample,
+    # NeverSample, and Probability. The Ruby implementation also provides a
+    # fourth, MaxQPS, based on the Stackdriver library.
+    #
+    # A sampler is a Proc that takes a hash of environment information and
+    # returns a boolean indicating whether or not to sample the current
+    # request. Alternately, it could be an object that duck-types the Proc
+    # interface by implementing the `call` method. The hash passed to `call`
+    # may contain the following keys, all of which are optional. Samplers must
+    # adjust their behavior to account for the availability or absence of any
+    # environment information:
+    #
+    # *   `span_context` The SpanContext that created the span being sampled.
+    # *   `rack_env` The hash of Rack environment information
+    #
+    # Applications may set a default sampler in the config. In addition, the
+    # sampler may be overridden whenever a span is created.
+    #
     module Samplers
-      DEFAULT = Probability.new Probability::DEFAULT_RATE
     end
   end
 end

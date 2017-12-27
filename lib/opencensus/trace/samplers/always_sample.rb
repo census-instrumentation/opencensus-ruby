2efdc488b6b601295fe5f60d606a5f046ebf92e8
Jeff Ching <ching.jeff@gmail.com>
2017-12-27 10:59:33 -0800

    Add docs for the sampler implementations (#19)

--- lib/opencensus/trace/samplers/always_sample.rb
+++ lib/opencensus/trace/samplers/always_sample.rb
@@ -15,7 +15,16 @@
 module OpenCensus
   module Trace
     module Samplers
+      ##
+      # The AlwaysSample sampler always returns true.
+      #
       class AlwaysSample
+        ##
+        # Implements the sampler contract. Checks to see whether a sample
+        # should be taken at this time.
+        #
+        # @return [boolean] Whether to sample at this time.
+        #
         def call _opts = {}
           true
         end

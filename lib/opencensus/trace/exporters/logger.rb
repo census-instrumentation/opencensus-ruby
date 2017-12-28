d8739ce32295a07084a3d0305e9013b75d511da0
Jeff Ching <ching.jeff@gmail.com>
2017-12-27 17:01:03 -0800

    Logger exporter (#24)
    * Add logger documentation, fix namespace
    
    * Implement logger exporter with tests
    
    * Fix rubocop

--- lib/opencensus/trace/exporters/logger.rb
+++ lib/opencensus/trace/exporters/logger.rb
@@ -13,18 +13,107 @@
 # limitations under the License.
 
 require "logger"
+require "json"
 
 module OpenCensus
   module Trace
     module Exporters
+      ##
+      # The Logger exporter exports captured spans to a standard Ruby Logger
+      # interface.
+      #
       class Logger
-        def initialize logger, level: Logger::INFO
+        ##
+        # Create a new Logger exporter
+        #
+        # @param [#log] logger The logger to write to.
+        # @param [Symbol] level The log level. This should be a log level
+        #        defined by {https://ruby-doc.org/stdlib-2.5.0/libdoc/logger/rdoc/Logger.html
+        #        Logger Standard Library}.
+        #
+        def initialize logger, level: ::Logger::INFO
           @logger = logger
           @level = level
         end
 
-        def report trace
-          logger.log(@level, trace.to_json)
+        ##
+        # Export the captured spans to the configured logger.
+        #
+        # @param [Array<Span>] spans The captured spans.
+        # @return [Boolean]
+        #
+        def export spans
+          @logger.log @level, spans.map { |span| format_span(span) }.to_json
+        end
+
+        private
+
+        def format_span span
+          {
+            name: span.name,
+            trace_id: span.trace_id,
+            span_id: span.span_id,
+            parent_span_id: span.parent_span_id,
+            start_time: span.start_time,
+            end_time: span.end_time,
+            attributes: span.attributes,
+            dropped_attributes_count: span.dropped_attributes_count,
+            stack_trace: span.stack_trace,
+            dropped_frames_count: span.dropped_frames_count,
+            time_events: span.time_events.map { |te| format_time_event(te) },
+            dropped_annotations_count: span.dropped_annotations_count,
+            dropped_message_events_count: span.dropped_message_events_count,
+            links: span.links.map { |link| format_link(link) },
+            dropped_links_count: span.dropped_links_count,
+            status: format_status(span.status),
+            same_process_as_parent_span: span.same_process_as_parent_span,
+            child_span_count: span.child_span_count
+          }
+        end
+
+        def format_time_event time_event
+          case time_event
+          when Annotation
+            format_annotation time_event
+          when MessageEvent
+            format_message_event time_event
+          end
+        end
+
+        def format_annotation annotation
+          {
+            description: annotation.description,
+            attributes: annotation.attributes,
+            time: annotation.time
+          }
+        end
+
+        def format_message_event message_event
+          {
+            type: message_event.type,
+            id: message_event.id,
+            uncompressed_size: message_event.uncompressed_size,
+            compressed_size: message_event.compressed_size,
+            time: message_event.time
+          }
+        end
+
+        def format_link link
+          {
+            trace_id: link.trace_id,
+            span_id: link.span_id,
+            type: link.type,
+            attributes: link.attributes
+          }
+        end
+
+        def format_status status
+          return nil if status.nil?
+
+          {
+            code: status.code,
+            message: status.message
+          }
         end
       end
     end

59032eb15ea416faf0e5275a2fe0e2126810e7d9
Jeff Ching <chingor@google.com>
2017-12-28 09:20:50 -0800

    Add exporter config and tests

--- test/trace/integrations/rack_middleware_test.rb
+++ test/trace/integrations/rack_middleware_test.rb
@@ -37,9 +37,10 @@ describe OpenCensus::Trace::Integrations::RackMiddleware do
 
   let(:app) { TestApp.new }
   let(:exporter) { TestExporter.new }
-  let(:middleware) { OpenCensus::Trace::Integrations::RackMiddleware.new app, exporter: exporter }
 
   describe "basic request" do
+    let(:middleware) { OpenCensus::Trace::Integrations::RackMiddleware.new app, exporter: exporter }
+
     it "parses the request path" do
       env = {
         "SCRIPT_NAME" => "",
@@ -50,6 +51,44 @@ describe OpenCensus::Trace::Integrations::RackMiddleware do
       spans = exporter.spans
       spans.wont_be_empty
       spans.count.must_equal 1
+    end
+  end
+
+  describe "global configuration" do
+    describe "default exporter" do
+      let(:middleware) { OpenCensus::Trace::Integrations::RackMiddleware.new app }
+
+      it "should use the default Logger exporter" do
+        env = {
+          "SCRIPT_NAME" => "",
+          "PATH_INFO" => "/hello/world"
+        }
+        out, _err = capture_subprocess_io do
+          middleware.call env
+        end
+        out.wont_be_empty
+      end
+    end
+
+    describe "custom exporter" do
+      before do
+        @original_exporter = OpenCensus::Trace::Config.exporter
+        OpenCensus::Trace::Config.exporter = exporter
+      end
+      after { OpenCensus::Trace::Config.exporter = @original_exporter }
+      let(:middleware) { OpenCensus::Trace::Integrations::RackMiddleware.new app }
+
+      it "should capture the request" do
+        env = {
+          "SCRIPT_NAME" => "",
+          "PATH_INFO" => "/hello/world"
+        }
+        middleware.call env
+
+        spans = exporter.spans
+        spans.wont_be_empty
+        spans.count.must_equal 1
+      end
     end
   end
 end

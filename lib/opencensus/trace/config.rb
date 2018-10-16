6548e918b1d8b3758e6ab271e141ae061bff3106
Bing Han <h.bing612@gmail.com>
2018-10-16 08:13:40 +0800

    Fix require missing in trace (#70)

--- lib/opencensus/trace/config.rb
+++ lib/opencensus/trace/config.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "opencensus/config"
 require "opencensus/trace/exporters"
 require "opencensus/trace/formatters"
 require "opencensus/trace/samplers"
